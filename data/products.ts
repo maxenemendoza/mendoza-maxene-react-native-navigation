a0ffdc56652a9ff07ae83b21e8eeded80309b8d9
Maxene Sofia Mendoza <maxene_sofia_mendoza@dlsl.edu.ph>
2026-02-06 20:21:02 +0800

    fixed assets in products.ts for expo.dev

--- data/products.ts
+++ data/products.ts
@@ -1,46 +1,51 @@
-import type { Product} from '../types/index';
+import snackImg from '../assets/SNACK.png';
+import loveImg from '../assets/LOVE.png';
+import pumpkinImg from '../assets/PUMPKIN.png';
+import dogImg from '../assets/DOG.png';
+import santaImg from '../assets/SANTA.png';
+import catImg from '../assets/CAT.png';
 
-export const products: Product[] = [
+export const products = [
   {
     id: 1,
     name: 'Snack Series',
     price: 15,
     description: 'Just like dining at a retro American diner, Sonny Angel brings joy to every mealtime moment.',
-    image: require('../assets/SNACK.png'),
+    image: snackImg,
   },
   {
     id: 2,
     name: 'Gifts of Love Series',
     price: 20,
     description: 'The Gifts of Love series is a message of “I love you!” from Sonny Angel and represents his daily gratitude and affection.',
-    image: require('../assets/LOVE.png'),
+    image: loveImg,
   },
   {
     id: 3,
     name: 'Pumpkin Patch Series',
     price: 20,
     description: 'Sonny Angel dons colorful, playful pumpkin costumes in the brand new Pumpkin Patch Series.',
-    image: require('../assets/PUMPKIN.png'),
+    image: pumpkinImg,
   },
   {
     id: 4,
     name: 'Dog Time Series',
     price: 20,
     description: 'RUFF! Make Every Moment More Playful with the Sonny Angel Dog Time Series!',
-    image: require('../assets/DOG.png'),
+    image: dogImg,
   },
   {
     id: 5,
     name: "Santa's Little Helper Series",
     price: 30,
     description: 'Get Ready for Christmas Celebrations with Sonny Angel.',
-    image: require('../assets/SANTA.png'),
+    image: santaImg,
   },
   {
     id: 6,
     name: 'Cat Life Series',
     price: 20,
     description: 'Add fun to every day with the Sonny Angel Cat Life series.',
-    image: require('../assets/CAT.png'),
+    image: catImg,
   },
 ];
